5632473a58595af2ab7db69da5e18b03b3254f36
sangmin <xectler@gmail.com>
2023-05-17 15:30:50 +0900

    add image

--- backend/src/routes/subs.ts
+++ backend/src/routes/subs.ts
@@ -1,5 +1,4 @@
-import { Request, Response, Router } from "express";
-import jwt from 'jsonwebtoken'
+import { NextFunction, Request, Response, Router } from "express";
 import { User } from "../entities/User";
 import userMiddleware from "../middlewares/user"
 import authMiddleware from "../middlewares/auth"
@@ -7,6 +6,10 @@ import { isEmpty } from "class-validator";
 import { AppDataSource } from "../data-source";
 import Sub from "../entities/Sub";
 import Post from "../entities/Post";
+import multer, { FileFilterCallback } from "multer";
+import { makeId } from "../utils/helper";
+import path from "path";
+import { unlinkSync } from "fs";
 
 const createSub = async (req: Request, res: Response, next) => {
     const { name, title, description } = req.body
@@ -55,7 +58,7 @@ const createSub = async (req: Request, res: Response, next) => {
 const topSubs = async (_: Request, res: Response) => {
     
     try {
-        const imageUrlExp = `COALESCE(s."imageUrn", 'https://www.gravatar.com/avatar?d=mp&f=y')`
+        const imageUrlExp = `COALESCE('${process.env.APP_URL}/images/' || s."imageUrn", 'https://www.gravatar.com/avatar?d=mp&f=y')`
         const subs = await AppDataSource.createQueryBuilder()
         .select(
             `s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`
@@ -86,10 +89,101 @@ const getSub = async (req: Request, res: Response) => {
     }
 }
 
+const ownSub = async (req: Request, res: Response, next: NextFunction) => {
+    const user: User = res.locals.user
+
+    try {
+        const sub = await Sub.findOneOrFail({ where: { name: req.params.name } })
+
+        if(sub.username !== user.username) {
+            return res.status(403).json({ error: "이 커뮤니티를 소유하고 있지 않습니다." })
+        }
+
+        res.locals.sub = sub
+        next()
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "문제가 발생했습니다." })
+    }
+}
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: "public/images",
+        filename: (_, file, callback) => {
+            const name = makeId(10)
+            callback(null, name + path.extname(file.originalname))
+        },
+    }),
+    fileFilter: (_, file: any, callback: FileFilterCallback) => {
+        if(file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+            callback(null, true)
+        } else {
+            callback(new Error("이미지가 아닙니다."))
+        }
+    }
+})
+
+const uploadSubImage = async (req: Request, res: Response) => {
+    const sub: Sub = res.locals.sub
+
+    try {
+        const type = req.body.type
+        
+        // 파일 유형을 지정치 않았을 시에는 업로드된 파일 삭제
+        if(type !== "image" && type !== "banner") {
+            if(!req.file?.path) {
+                return res.status(400).json({
+                    error: "유효하지 않은 파일"
+                })
+            }
+            // 파일을 지워주기
+            unlinkSync(req.file.path)
+            return res.status(400).json({ error: "잘못된 유형" })
+        }
+
+        let oldImageUrn: string = ""
+
+        if(type === "image") {
+            // 이미 사용중인 Urn을 저장합니다. (이전 파일을 아래서 삭제하기 위해서)
+            oldImageUrn = sub.imageUrn || ""
+
+            // 새로운 파일 이름을 Urn 으로 넣어줍니다.
+            sub.imageUrn = req.file?.filename || ""
+        } else if(type === "banner") {
+            oldImageUrn = sub.bannerUrn || ""
+            sub.bannerUrn = req.file?.filename || ""
+        } 
+
+        await sub.save()
+
+        // 사용하지 않는 이미지 파일 삭제
+        if(oldImageUrn !== "") {
+            const fullFilename = path.resolve(
+                process.cwd(),
+                "public",
+                "images",
+                oldImageUrn
+            )
+
+            unlinkSync(fullFilename)
+        }
+
+        return res.json(sub)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ error: "문제가 발생했습니다." })
+    }
+}
+
 const router = Router()
 
 router.get("/:name", userMiddleware, authMiddleware, getSub)
+router.post("/:name/upload", userMiddleware, authMiddleware, ownSub, upload.single("file"), uploadSubImage)
 router.post("/", userMiddleware, authMiddleware, createSub)
 router.get("/sub/topSubs", topSubs) 
 
 export default router
+
+// npm install multer --save
+// npm i --save-dev @types/multer
